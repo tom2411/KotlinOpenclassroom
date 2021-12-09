77ce4ef6ba20b02153150dee92e88cc2b1f71777
Tom Ribardiere <tom.ribardiere@free.fr>
2021-12-09 17:31:05 +0100

    [ADD] Début de l'exo1

---
+++ src/variables/Exercice1.kt
@@ -0,0 +1,22 @@
+package variables
+
+class Exercice1 {
+    const BEST_PROGRAMMING_LANGUAGE = "Kotlin"
+
+    fun main() {
+
+        val hello = "Hello"
+        hello = "Hello world!"
+
+        println(hello)
+
+        var toto:Int = "Toto"
+
+        println(toto)
+
+        var message: String? = "I’m learning Kotlin!"
+        message = null
+        println(message.toString())
+
+    }
+}
