353d0df16a2626b2fe232d065b89aa2bfe5c993b
Tom Ribardiere <tom.ribardiere@free.fr>
2021-12-09 18:33:09 +0100

    [ADD] Fin du cours 2

---
+++ src/function/cours2.kt
@@ -0,0 +1,51 @@
+package function
+
+class cours2 {
+
+    // La définition d'une fonction en Kotlin
+    private fun main(args: Array<String>) {
+        println("Hello, world!")
+    }
+
+    // En Kotlin on peut retourner un if
+    // car c'est une expression et non une instruction comme en Java
+    // le type qui se trouve après les parentheses est le type de retour
+    // de la fonction
+    private fun minOf(a: Int, b: Int): Int {
+        return if(a < b ) a else b
+    }
+
+
+    private fun main2(args: Array<String>) {
+        println("Hello, world!")
+    }
+
+    // Unit est le type de retour par défaut
+    // équivalent à void en Java
+    // Quasiment jamais écrit grâce à l'inférence de type
+    var hello: Unit = main(arrayOf(""))
+
+    // On peut également coder des fonctions qui n'ont pas de corps
+    private fun minOf2(a: Int, b: Int): Int = if (a < b) a else b
+    // ou plus lisible
+    private fun minOf3(a: Int, b: Int) = if (a < b) a else b
+
+    // Ces 3 fonctions sont équivalentes
+    fun sayHello1() = println("Hello !")
+
+    fun sayHello2(): Unit = println("Hello !")
+
+    fun sayHello3(): Unit { println("Hello !") }
+
+    // 😱
+    // L'inférence de type ne fonctionne pas si nous avons un corps de fonction
+    // c'est pour optimiser les performances du compilateur
+    fun getUrlApi1() { return "https://www.my.api.com" }
+
+    // 🙂
+    fun getUrlApi2(): String { return "https://www.my.api.com" }
+
+    // 😎
+    fun getUrlApi3() = "https://www.my.api.com"
+
+}
