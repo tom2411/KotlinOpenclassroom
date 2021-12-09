11c3ebdf41e839c5a54513cd245e8640471ffc8f
Tom Ribardiere <tom.ribardiere@free.fr>
2021-12-09 21:57:05 +0100

    [ADD] Commencement de l'exo du cours 5

---
+++ src/Cours5_boucles/Exercice5.kt
@@ -0,0 +1,24 @@
+package Cours5_boucles
+
+class Exercice5 {
+
+    fun showList(list: List<String>){
+        // Complétez la fonction pour afficher les éléments de la liste
+    }
+
+    fun oddNumbersTo10(){
+        // Complétez la fonction pour afficher les nombres impairs jusqu'à 10
+    }
+
+    private val languages =  // Créez une liste ordonnée de plusieurs languages de programation
+
+    fun main(args: Array<String>) {
+        println("Languages :")
+        showList(languages)
+        println("Odd Numbers to 10 :")
+        oddNumbersTo10()
+    }
+
+
+
+}
